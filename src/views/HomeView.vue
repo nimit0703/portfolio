5a6649665a6ce6790f5c1586d59e27b85b0f52fa
nimit0703 <nimit.190410107143@gmail.com>
2024-02-11 19:29:28 +0530

    added skillcard modified css for selction

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,23 +6,53 @@
       </h3>
     </div>
     <div class="row">
-      <div class="bio-section col-8">
-        <div class="row bio-card col-6">
+      <div class="bio-section col-9">
+        <div class="row bio-card col-12">
           <div class="bio-img col-4 d-flex justify-content-center border-5 border-success">
             <img src="/images/avtar_img.png" class="img-logo">
           </div>
           <div class="bio-text col-6 d-flex flex-column">
-            <h6>Biography</h6>
-            <p class="text-secondary">Lorem iadssdpsum dolor sit amet consectetur adipisicing elit. Culpa, placeat!</p>
+            <p class="text-title">Biography</p>
+            <p class="text-gray">{{ intro }}</p>
           </div>
         </div>
       </div>
       <div class="connect-section d-flex flex-column col-3">
-        <h6 class="">Let's connect</h6>
+        <h6 class="text-title">Let's connect</h6>
         <div class="d-flex mt-4">
           <i class="bi fs-4 bi-instagram mr-5"></i>
           <i class="bi fs-4 bi-linkedin mr-5"></i>
           <i class="bi fs-4 bi-github mr-5"></i>
+        </div>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col-4 d-flex flex-column">
+        <div class="">
+          <p class="text-title"></p> What i do?
+        </div>
+        <div class="mt-3">
+          <p class="text-gray">
+            {{ intro }}
+          </p>
+        </div>
+      </div>
+      <div class="col-8 p-2">
+        <div class="container">
+          <div class="row ">
+            <SkillCardHome 
+            :icon="'bi bi-github'"
+            :title="'front-end'"
+            :desc="intro"
+            ></SkillCardHome>
+            
+            <SkillCardHome
+            :icon="'bi bi-github'"
+            :title="'front-end'"
+            :desc="intro"
+            ></SkillCardHome>
+          </div>
         </div>
       </div>
     </div>
@@ -31,6 +61,6 @@
   
 <script setup lang="ts">
 import { intro } from '@/module/constant/constant.ts'
-
+import SkillCardHome from "@/components/common/cards/SkillCardHome.vue";
 </script>
   
